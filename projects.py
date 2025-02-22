a84e3838750b1c6f48dbe02475b1598dcbfda403
abdelhak-25 <abdelhakmorhlia@gmail.com>
2025-02-22 14:05:58 +0100

    Delete projects.py

--- projects.py
+++
@@ -1,9 +0,0 @@
-import json
-print('hello SR')
-name=input('Please enter your name :')
-age=int(input('please enter your age :'))
-
-r=f'hi {name} your age is {age}'
-with open('user','w')as file:
-    json.dump(r,file,indent=4)
-    print('data saved succesfully')
